2a8283276e63accff018549783f1595a9b07ce57
Anatoliy Litvinov <program.virus.login@gmail.com>
2021-02-19 05:07:05 +0200

    Added description for received data from api

--- src/config/unsplash.config.ts
+++ src/config/unsplash.config.ts
@@ -1,6 +1,5 @@
 export const unsplashConfig = {
   "api_baseUrl": "https://api.unsplash.com",
   "api_urlPhotos": "/photos",
-  "api_urlPhoto": "/photo",
   "access_key": "cf49c08b444ff4cb9e4d126b7e9f7513ba1ee58de7906e4360afc1a33d1bf4c0"
 }

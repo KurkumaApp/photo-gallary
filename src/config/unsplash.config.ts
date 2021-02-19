6c4a821629105f77bc13de7a0f455989d46c8ea9
Anatoliy Litvinov <program.virus.login@gmail.com>
2021-02-19 09:32:07 +0200

    Deleted unsplash config

--- src/config/unsplash.config.ts
+++
@@ -1,5 +0,0 @@
-export const unsplashConfig = {
-  "api_baseUrl": "https://api.unsplash.com",
-  "api_urlPhotos": "/photos",
-  "access_key": "cf49c08b444ff4cb9e4d126b7e9f7513ba1ee58de7906e4360afc1a33d1bf4c0"
-}
